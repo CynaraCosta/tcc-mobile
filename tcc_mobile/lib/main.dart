5ac9b1d806053ddf524cd9e3b9f4ccef3d07dd54
CynaraCosta <cvocac@cin.ufpe.br>
2024-11-09 15:43:50 -0300

    add root file

--- tcc_mobile/lib/main.dart
+++ tcc_mobile/lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
-import 'package:tcc_mobile/commons/debugging/debugging.dart';
-import 'package:tcc_mobile/commons/notifier/brightness_notifier.dart';
 import 'package:tcc_mobile/commons/router/router.dart';
 import 'package:tcc_mobile/di/injection.dart';
+import 'package:tcc_mobile/root.dart';
 import 'package:tcc_mobile/soma/soma.dart';
 
 void main() {
@@ -16,77 +13,4 @@ void main() {
       somaThemeData: GetIt.I.get<SomaThemeData>(),
     ),
   );
-}
-
-class TccMobile extends StatefulWidget {
-  const TccMobile({
-    required this.router,
-    required this.somaThemeData,
-    super.key,
-  });
-
-  final SomaThemeData somaThemeData;
-  final AppRouterConfig router;
-
-  @override
-  State<TccMobile> createState() => _TccMobileState();
-}
-
-class _TccMobileState extends State<TccMobile> with WidgetsBindingObserver {
-  late SomaThemeModeNotifier _themeModeNotifier;
-  late final WidgetsBinding _widgetsBinding;
-
-  @override
-  void initState() {
-    super.initState();
-    _widgetsBinding = WidgetsBinding.instance;
-    _widgetsBinding.addObserver(this);
-
-    final brightness = PlatformDispatcher.instance.platformBrightness;
-
-    _themeModeNotifier = SomaThemeModeNotifier(
-      ValueNotifier<Brightness>(
-        brightness,
-      ),
-    );
-  }
-
-  @override
-  void didChangePlatformBrightness() {
-    final brightness = PlatformDispatcher.instance.platformBrightness;
-
-    _themeModeNotifier.changeBrightness(
-      brightness: brightness,
-    );
-    super.didChangePlatformBrightness();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
-    return ValueListenableBuilder(
-      valueListenable: _themeModeNotifier.appBrightness,
-      builder: (context, value, child) {
-        final debuggingFloating = DebuggingFactory.buildDebugging(
-          () {
-            // ignore: avoid_print
-            print('clicou $value');
-          },
-        );
-        bool isInverse = (value == Brightness.dark) ? true : false;
-        return SomaTheme(
-          data: widget.somaThemeData,
-          child: SomaContext(
-            isInverse: isInverse,
-            child: MaterialApp.router(
-              builder: debuggingFloating.createBuilderFloating,
-              routerConfig: widget.router,
-            ),
-          ),
-        );
-      },
-    );
-  }
 }
---
+++ tcc_mobile/lib/root.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tcc_mobile/commons/debugging/debugging.dart';
+import 'package:tcc_mobile/commons/notifier/brightness_notifier.dart';
+import 'package:tcc_mobile/commons/router/router.dart';
+import 'package:tcc_mobile/soma/soma.dart';
+
+class TccMobile extends StatefulWidget {
+  const TccMobile({
+    required this.router,
+    required this.somaThemeData,
+    super.key,
+  });
+
+  final SomaThemeData somaThemeData;
+  final AppRouterConfig router;
+
+  @override
+  State<TccMobile> createState() => _TccMobileState();
+}
+
+class _TccMobileState extends State<TccMobile> with WidgetsBindingObserver {
+  late SomaThemeModeNotifier _themeModeNotifier;
+  late final WidgetsBinding _widgetsBinding;
+
+  @override
+  void initState() {
+    super.initState();
+    _widgetsBinding = WidgetsBinding.instance;
+    _widgetsBinding.addObserver(this);
+
+    final brightness = PlatformDispatcher.instance.platformBrightness;
+
+    _themeModeNotifier = SomaThemeModeNotifier(
+      ValueNotifier<Brightness>(
+        brightness,
+      ),
+    );
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    final brightness = PlatformDispatcher.instance.platformBrightness;
+
+    _themeModeNotifier.changeBrightness(
+      brightness: brightness,
+    );
+    super.didChangePlatformBrightness();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    return ValueListenableBuilder(
+      valueListenable: _themeModeNotifier.appBrightness,
+      builder: (context, value, child) {
+        final debuggingFloating = DebuggingFactory.buildDebugging(
+          () {
+            // ignore: avoid_print
+            print('clicou $value');
+          },
+        );
+        bool isInverse = (value == Brightness.dark) ? true : false;
+        return SomaTheme(
+          data: widget.somaThemeData,
+          child: SomaContext(
+            isInverse: isInverse,
+            child: MaterialApp.router(
+              builder: debuggingFloating.createBuilderFloating,
+              routerConfig: widget.router,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
