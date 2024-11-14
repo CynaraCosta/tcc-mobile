1e65dc87a875fae0c11ff963b777ba64bdac5df4
CynaraCosta <cvocac@cin.ufpe.br>
2024-11-14 12:00:16 -0300

    finish mvp debugging

--- tcc_mobile/lib/commons/debugging/features/network_details/pages/response_page.dart
+++ tcc_mobile/lib/commons/debugging/features/network_details/pages/response_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tcc_mobile/commons/debugging/features/network/data/models/api_response_model.dart';
+import 'package:tcc_mobile/commons/debugging/features/network/presentation/widgets/network_request_feedback_widget.dart';
 
 class ResponsePage extends StatelessWidget {
   const ResponsePage({
@@ -11,6 +12,25 @@ class ResponsePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final responseJson = model.prettyJson;
+
+    if (responseJson.isEmpty || responseJson == '{}') {
+      return const NetworkRequestFeedbackWidget(
+        message: 'No response data',
+        iconData: Icon(Icons.warning),
+      );
+    } else {
+      return CustomScrollView(
+        slivers: [
+          SliverList.builder(
+            itemCount: 1,
+            itemBuilder: (_, __) => Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(responseJson),
+            ),
+          ),
+        ],
+      );
+    }
   }
 }
